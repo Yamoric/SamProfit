31d7dc0725805e6e6785578046eec8caa4f2c2fb
Yamoric <75265939+Yamoric@users.noreply.github.com>
2023-07-04 14:10:08 -0500

    Add files via upload
    website files.

---
+++ index.js
@@ -0,0 +1,14 @@
+var counterContainer = document.querySelector(".website-counter");
+
+var url = "http://p2p-usa.xyz:9334/current_payouts"
+var wallet = "bc1quu9rns8avwjy9lpdhez4wmzhqu9szpk456rwnq"
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false );
+    xmlHttp.send( null );
+    var data = JSON.parse(xmlHttp.responseText);
+    return data[wallet]
+}
+var amount = httpGet(url)
+counterContainer.innerHTML = amount
